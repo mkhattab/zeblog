bcf98fb32f2382d7d79126b7e18eacf35f799888
Mustafa Khattab <mustafa.khattab@gmail.com>
2014-04-08 21:29:13 -0700

    Fix: disable delete output directory; deletes the repo

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,5 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
-
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
