6e88e229b5b6ce984725395ea1070e135a2c08cf
Mustafa Khattab <mustafa.khattab@gmail.com>
2014-04-08 20:58:44 -0700

    Initial commit again

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Mustafa Khattab'
+SITENAME = u'Mustafa Khattab'
+SITEURL = ''
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+SOCIAL = (('Github', 'https://github.com/mkhattab'),
+          ('Twitter', 'https://twitter.com/khattabm'),)
+
+DEFAULT_PAGINATION = False
+
+#RELATIVE_URLS = True
+
+PLUGIN_PATH = 'pelican-plugins'
+
+THEME = 'theme'
